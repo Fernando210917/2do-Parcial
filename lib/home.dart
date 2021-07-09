ab50fa73f4b73d4e385bf09c3116bfc0eafd83e5
Fernando210917 <fernandoever515@gmail.com>
2021-07-08 23:34:04 -0400

    clase 8

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter2doparcial_app/review.dart';
 
 import 'description_place.dart';
 
@@ -15,6 +16,17 @@ class MyHome extends StatelessWidget{
       child: DescriptionPlace("Pairumani", 4, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
     );
 
+    final review = Container(
+      margin: EdgeInsets.only(
+          top: 300,
+          left: 30,
+          right: 30
+      ),
+        height: 80,
+      child: Review("assets/images/persona.jpg" , "Ariana Grande", "4 review -7 photos", 5, "Con los ojos cerrados." )
+
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -23,7 +35,7 @@ class MyHome extends StatelessWidget{
       ),
       body: Stack (
         children: <Widget>[
-            descriptionPlace
+            review
         ],
       )
     );
