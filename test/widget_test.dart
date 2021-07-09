adb9f7bfbbe160c68a4eacc884ff7d6486d9a9bb
Fernando210917 <fernandoever515@gmail.com>
2021-07-08 23:25:31 -0400

    clase 7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:practica_2do_parcial/main.dart';
+import 'package:flutter2doparcial_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
